a2716470e98b1f03576fe78ad9340ca64d90c6c7
T-ZhenLS <zh3nl5.20@gmail.com>
2021-06-28 15:54:11 +0800

    Button & OnClickListener

--- app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
+++ app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.practical_mobileappdevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Target: change textView bg color according to btn selected
+         */
+
+        // Get elements textView and buttons
+        View.OnClickListener colorChangeListener;
+        TextView textView = findViewById(R.id.txt_pick);
+        RadioButton radioBtnPrimrose = findViewById(R.id.radio_primrose);
+        RadioButton radioBtnHoneydew = findViewById(R.id.radio_honeydew);
+        RadioButton radioBtnApricot = findViewById(R.id.radio_apricot);
+        RadioButton radioBtnPoppy = findViewById(R.id.radio_poppy);
+
+        // Define colorChangeListener
+        colorChangeListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                RadioButton temp = (RadioButton) v;
+                switch (v.getId()){
+                    case R.id.radio_primrose:
+                        textView.setBackgroundColor(getResources().getColor(R.color.primrose));
+                        break;
+                    case R.id.radio_apricot:
+                        textView.setBackgroundResource(R.color.apricot);
+                        break;
+                    case R.id.radio_honeydew:
+                        textView.setBackgroundResource(R.color.honeydew);
+                        break;
+                    case R.id.radio_poppy:
+                        textView.setBackgroundColor(getResources().getColor(R.color.poppy));
+                        break;
+                    default:
+                        break;
+                }
+                textView.setTextColor(Color.BLACK);
+            }
+        };
+
+        // Bind colorChangeListener to radioBtn
+        radioBtnApricot.setOnClickListener(colorChangeListener);
+        radioBtnPrimrose.setOnClickListener(colorChangeListener);
+        radioBtnHoneydew.setOnClickListener(colorChangeListener);
+        radioBtnPoppy.setOnClickListener(colorChangeListener);
     }
 }

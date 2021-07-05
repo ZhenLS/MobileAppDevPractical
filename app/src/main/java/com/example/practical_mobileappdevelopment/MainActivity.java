e062b456726baee3f92fd1887cc49d03327e8ef5
T-ZhenLS <zh3nl5.20@gmail.com>
2021-07-05 16:19:14 +0800

    Draw Shape in Custom View

--- app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
+++ app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
@@ -8,6 +8,10 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,70 +24,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+
         setContentView(new CustomView(this));
     }
 
     private static class CustomView extends View {
+
+        private ShapeDrawable drawable;
+
         public CustomView(Context context) {
             super(context);
+
+            // Method 1: Define Rectangle
+            int x = 100;
+            int y = 100;
+            int width = 600;
+            int height = 100;
+
+            drawable = new ShapeDrawable(new RectShape());
+            drawable.getPaint().setColor(Color.parseColor("#DAB1AD"));
+            drawable.setBounds(x, y, x + width, y + height);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {      // Canvas == drawing pad
             /**
-             * When using Canvas:
-             * 1. Set Background Color
-             * 2. Set Paint (Line/Shape) Color
-             * 3. Setup Fonts
-             * 4. Draw primitive operations / Shape
+             * Exercise 3: Draw Rectangle
+             * Define Rectangle in constructor. draw in onDraw
              */
-
-            // Set BG Color
-            canvas.drawColor(Color.DKGRAY);
-
-            // Set Paint Color
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);     // Anti alias: replace the jagger line with smooth line
-
-            // 2nd Component: Fonts
-            Typeface typeface;
-            typeface = Typeface.create(Typeface.MONOSPACE, Typeface.NORMAL);
-
-            paint.setColor(Color.GREEN);
-            paint.setTextSize(40);
-            paint.setTypeface(typeface);
-
-            // 3rd component: drawing primitive operations / shape
-            canvas.drawText("This is my Low-Level_UI using View.",
-                    10, 50, paint);
-
-            // Second Format
-            paint.setColor(Color.parseColor("aqua"));
-            Typeface typeface1 = Typeface.create(Typeface.SERIF, Typeface.BOLD_ITALIC);
-            paint.setTypeface(typeface1);
-            canvas.drawText("2ND Low LEVEL UI",
-                    10, 100, paint);
-
-            /**
-             * Exercise 2: Add Picture
-             * 1. Create Bitmap
-             * 2. Add Intended Transformation
-             * 3. Use canvas.drawBitmap() to draw
-             */
-            Bitmap picture = BitmapFactory.decodeResource(getResources(),R.drawable.docker_icon);
-
-            // Transformation: Scaling, Translation, Rotation, Reflection
-            Matrix transMatrix = new Matrix();
-            transMatrix.preScale(2.0f, 2.0f);   // Scaling Up
-            transMatrix.preScale(-1.0f, 1.0f); // Inverse position
-            transMatrix.preRotate(15);
-
-            // Create Transformed Picture Based on original picture
-            Bitmap transformedPic = Bitmap.createBitmap(picture,
-                    0,0,picture.getWidth(),picture.getHeight(),
-                    transMatrix,false);
-
-            // Draw Picture on Canvas
-            canvas.drawBitmap(transformedPic,10,150,null);
+            drawable.draw(canvas);
         }
     }
 }

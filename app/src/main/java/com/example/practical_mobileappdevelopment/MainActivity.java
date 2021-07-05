2a81a740a42ec5535f192c68a554835e701a2763
T-ZhenLS <zh3nl5.20@gmail.com>
2021-07-05 17:22:46 +0800

    Pacman Drawing - ImageView

--- app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
+++ app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
 import android.view.View;
@@ -19,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
 
-        setContentView(new CustomView(this));
+        /**
+         * Q4: Pacman Drawing - Arc shape, angle clockwise, startAngle & sweepAngle
+         */
+        ShapeDrawable pacMan = new ShapeDrawable(new ArcShape(40, 290));
+        pacMan.setIntrinsicWidth(1);
+        pacMan.setIntrinsicHeight(1);
+        pacMan.getPaint().setColor(Color.parseColor("#FFEE87"));
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(pacMan);
+
+        setContentView(imageView);
     }
 
     private static class CustomView extends View {
@@ -34,27 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onDraw(Canvas canvas) {      // Canvas == drawing pad
-            /**
-             * Drawing Pacman
-             * Method 1: using onDraw in CustomView
-             */
-
-            Paint pManPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            canvas.drawColor(Color.DKGRAY);
-
-            // Body of Pacman
-            pManPaint.setColor(Color.parseColor("#FFEE87"));
-            canvas.drawArc(new RectF(getWidth() / 2 - 200,
-                            200,
-                            getWidth() / 2 + 200, 600),
-                    40, 290,
-                    true, pManPaint);
-
-            // Eye of Pacman
-            pManPaint.setColor(Color.BLACK);
-            canvas.drawCircle(getWidth() / 2 + 50,
-                    getHeight() / 2 - 750,
-                    20, pManPaint);
         }
     }
 }

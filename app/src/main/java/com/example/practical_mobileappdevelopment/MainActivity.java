7cff64e4c01f1e2496341e9c008481ff095e56bf
T-ZhenLS <zh3nl5.20@gmail.com>
2021-08-09 12:56:31 +0800

    Drawing on Canvas

--- app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
+++ app/src/main/java/com/example/practical_mobileappdevelopment/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.practical_mobileappdevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+
+
+        Button clearViewBtn = findViewById(R.id.clearView);
+        clearViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TouchEventView teView = findViewById(R.id.touchView);
+                teView.clearScreen();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/practical_mobileappdevelopment/TouchEventView.java
@@ -0,0 +1,58 @@
+package com.example.practical_mobileappdevelopment;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class TouchEventView extends View {
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    public TouchEventView(Context context, AttributeSet attrs){
+        super(context, attrs);
+
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(6f);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                return false;
+        }
+
+        // Schedule a repaint
+        invalidate();
+        return true;
+    }
+
+    public void clearScreen() {
+        path.reset();
+        invalidate();
+    }
+}
